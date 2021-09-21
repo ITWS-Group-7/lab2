40204e1cf0063c1607aa774195ec72be9b71bfc2
chungs10 <chungs10@rpi.edu>
2021-09-21 17:45:21 -0400

    THe beginning of Quiz Project
    Setup HTML ahead of time, nothing in CSS yet, working on answers and questions for Trivia

---
+++ trivia.js
@@ -0,0 +1,25 @@
+var myQuestions = [
+    {
+      question: "What is the purpose of Article 1",
+      answers: {
+        a: 'If Federal laws are above state laws',
+        b: 'How the legislative branch works',
+        c: 'How the Judicial branch works',
+        d: 'How can the Constitution can be Changed'
+        
+      },
+      correctAnswer: 'b'
+    },
+    {
+      question: "What is Article 6 about?",
+      answers: {
+        a: 'How the states should function ',
+        b: 'How the US deals with Debt',
+        c: 'How can the Constitution can be Changed',
+        d: 'How the Judicial branch works'
+      },
+      correctAnswer: 'c'
+    }
+  ];
+  
+ 
