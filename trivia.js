6d57e158de59a1fa6045567da58e57bf0b857105
chungs10 <chungs10@rpi.edu>
2021-09-22 16:59:57 -0400

    Importing Questions from other group mates

--- trivia.js
+++ trivia.js
@@ -19,9 +19,118 @@ var Allquestions = [
         d: 'How the Judicial branch works'
       },
       correctAnswer: 'c'
+    },
+    {
+      question: "What is Amendment 1 about?",
+      answers: {
+        a: 'The right to bear arms ',
+        b: 'The right to not have your picture taken',
+        c: 'Freedom of religion and speech',
+        d: 'Not being allowed to disrespect people'
+      },
+      correctAnswer: 'c'
+    }
+    ,
+    {
+      question: "What is Amendment 2 about?",
+      answers: {
+        a: 'The right to bear arms ',
+        b: 'The right to not have your picture taken',
+        c: 'Freedom of religion and speech',
+        d: 'Not being allowed to disrespect people'
+      },
+      correctAnswer: 'a'
+    }
+    ,
+    {
+      question: "What is Amendment 3 about?",
+      answers: {
+        a: 'Protecting private homewoners from military invasion ',
+        b: 'The right to bear arms',
+        c: 'The right to be seen before the court of law',
+        d: 'The right to be outside late at night'
+      },
+      correctAnswer: 'a'
+    }
+    ,
+    {
+      question: "What is Amendment 4 about?",
+      answers: {
+        a: 'Protecting private homewoners from military invasion',
+        b: 'The right to bear arms',
+        c: 'The right to be seen before the court of law',
+        d: 'The right to not be unlawfully searched by the police without a justifiable reason'
+      },
+      correctAnswer: 'd'
+    }
+    ,
+    {
+      question: "What is Amendment 5 about?",
+      answers: {
+        a: 'The right to not be arrested',
+        b: 'The right to own a business',
+        c: 'Following the golden rule',
+        d: 'The right to not have to testify against oneself in court'
+      },
+      correctAnswer: 'd'
+    }
+    ,
+    {
+      question: "What is Amendment 6 about?",
+      answers: {
+        a: 'Being kind to fellow citizens',
+        b: 'Criminals still maintaining their rights such as the right to have a lawyer',
+        c: 'Doing charity work',
+        d: 'Protection from excessive punishments'
+      },
+      correctAnswer: 'b'
+    }
+
+    ,
+    {
+      question: "What is Amendment 7 about?",
+      answers: {
+        a: 'Businesses going bankrupt',
+        b: 'The right to have medical records protected',
+        c: 'Civil cases being settled in federal court',
+        d: 'Protecting private homewoners from military invasion'
+      },
+      correctAnswer: 'c'
+    }
+    ,
+    {
+      question: "What is Amendment 8 about?",
+      answers: {
+        a: 'Protecting criminals from excessive punishment such as unreasonably high bails',
+        b: 'The right to be seen before the court of law',
+        c: 'The right to be outside late at night',
+        d: 'Freedom of religion and speech'
+      },
+      correctAnswer: 'a'
+    }
+    ,
+    {
+      question: "What is Amendment 9 about?",
+      answers: {
+        a: 'Consequences for driving recklessly',
+        b: 'The right to publicly display artwork',
+        c: 'The right to be outside late at night',
+        d: 'Ensuring that people have more rights than those stated in the Constitution'
+      },
+      correctAnswer: 'd'
+    }
+    ,
+    {
+      question: "What is Amendment 10 about?",
+      answers: {
+        a: 'Civil cases being settled in federal court',
+        b: 'Ensuring the balance in power between the federal and state governments',
+        c: 'Businesses going bankrupt',
+        d: 'Avoding taxes'
+      },
+      correctAnswer: 'b'
     }
   ];
-  
  var quiz = document.getElementById ('quiz');
  var results = document.getElementById('results');
  var submit = document.getElementById('submit');
